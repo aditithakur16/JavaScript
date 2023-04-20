c19dd2edd1d2100f2bbfa32f2e981b5e213b36ef
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-20 20:37:48 +0530

    restart Js

---
+++ D1/C1.js
@@ -0,0 +1,33 @@
+alert("hello")
+console.log("Namste JavaScript🙏")
+
+// Variables
+
+//There are 4 types of variables in JavaScript
+//1. var
+//2. let
+//3. const
+//4. None
+
+
+//1.var--->
+//var a = 20; 
+//console.log(a);
+//Output=20
+
+
+//2.let--->
+//let a = 30; 
+//console.log(a);
+//Output = 30
+
+//3.const
+//const a = 10;
+//console.log(a);
+
+//4.None
+//x = 10;
+//console.log(x)
+//or
+x = "aditi"
+console.log(x);
---
+++ classOne.js
@@ -0,0 +1,28 @@
+//VARIABLES
+
+//4-TYPES OF VARIABLES
+//1. var
+//2. let
+//3. const
+//4. null
+
+
+//1. var:-
+
+//ex.1)
+// var a = 10;
+// console.log(a);
+
+//2. let:-
+//ex.1)
+// let a = 20;
+// console.log(a);
+
+//3. const:-
+// const a = 30;
+// console.log(a);
+
+//4. null:-
+// 
+
+let 
